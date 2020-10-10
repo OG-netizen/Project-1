0b398c3ad0465c6fd9314b1c6ff948d965022ac1
OG-netizen <70165988+OG-netizen@users.noreply.github.com>
2020-10-11 01:33:07 +0200

    Update slave.ino

--- slave.ino
+++ slave.ino
@@ -1,23 +1,37 @@
-
 #include <Wire.h>
-
+int IRSensor = 2; 
 void setup() {
   Wire.begin(8);                // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
+
+  pinMode (IRSensor, INPUT);
+  
 }
 
 
 
 byte data[1];
+
 void loop() {
-  delay(100);
+int statusSensor = digitalRead (IRSensor);
+  if (statusSensor == 1){
+    statusSensor = 0;
+  }
+  
+  else
+  {
+    statusSensor = 1;
+  }
+
+  data[0] = statusSensor;
+  
+
+  
 }
 
 // function that executes whenever data is requested by master
 // this function is registered as an event, see setup()
 void requestEvent() {
-
-  data[0] = 1;
 
   
   Wire.write(data,1); // respond with message of 6 bytes
