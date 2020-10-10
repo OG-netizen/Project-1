c4c4603010a42740e63cddfd252d58297f155d8e
OG-netizen <70165988+OG-netizen@users.noreply.github.com>
2020-10-11 00:24:40 +0200

    Update Master.ino

--- Master.ino
+++ Master.ino
@@ -1,34 +1,31 @@
 
+
+// Wire Master Reader
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Reads data from an I2C/TWI slave device
+// Refer to the "Wire Slave Sender" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+
 #include <Wire.h>
 
-
-int LED = 5;
-
-void setup()
-{
-  Wire.begin(); // join i2c bus (address optional for master)
-  pinMode(LED,OUTPUT);
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(9600);  // start serial for output
 }
 
-byte x = 0;
+void loop() {
+  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
 
-void loop()
-{
-  Wire.beginTransmission(4); // transmit to device #4
-  Wire.write(0);
-  Wire.endTransmission(); 
-  Wire.requestFrom(4, 1);
-
-
-  while(Wire.available() == 0);
-  
-  int IRpin= Wire.read();
-
-  if (IRpin == 1){
-    digitalWrite(LED,HIGH);   
-  }else{
-    digitalWrite(LED,LOW);
+  while (Wire.available()) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    Serial.print(c);         // print the character
   }
 
-  
+  delay(500);
 }
