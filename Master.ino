9cae5117d289fb206a948af397de92781d4860b7
OG-netizen <70165988+OG-netizen@users.noreply.github.com>
2020-11-05 14:02:24 +0100

    Add files via upload

--- Master.ino
+++ Master.ino
@@ -1,14 +1,23 @@
 #include <Wire.h>
 #include <Keypad.h>
 #include <Stepper.h>
-#define STEPS 32
+int number;
 
- int stepsPerRevolution = 10000; 
-Stepper stepper(STEPS, 8, 10, 9, 11);
+int steps = 0;
+boolean moveUp = false;   // Should the motor move clockwise
+boolean moveDown = false; // Should the motor move counter-clockwise
+
+boolean clockwise = true;
+// Setup stepper
+#define IN1 11
+#define IN2 10
+#define IN3 9
+#define IN4 8
 
 byte currentFloor = 0;
 int CONNECTED_SLAVES = 5;
-int val = 0;
+
+int goToFloor;
 
 int DoorNumber;
 const byte ROWS = 4; //four rows
@@ -28,16 +37,47 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);// start serial for output
-  pinMode(13,OUTPUT);
-    stepper.setSpeed(1000);
+ 
+  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
 }
 
 void loop() {
 
- 
 getAndSendDataToAllFloors();
-KeypadFunction();  
+ 
+KeypadFunction();
+
+Serial.println("THE CURRENT FLOOR IS");
+ Serial.println(currentFloor);  
+
+ Serial.println("THE GO TO FLOOR IS");
+ Serial.println(goToFloor);  
+
+
+
+ if (goToFloor > currentFloor) {
+    Serial.println("lift gaat omhoog");
+    moveUp = true;                      
+    moveDown = false;     //lift moving up
+  }
+  if (goToFloor < currentFloor) {
+    Serial.println("lift gaat omlaag");
+    moveUp = false;
+    moveDown = true;      //lift moving down
+  }
+  if (goToFloor == currentFloor) {
+    Serial.println("lift is op bestemming");
+
+    moveUp = false;
+    moveDown = false;
+  }
+
+  moveLift();
   
 
 }
@@ -54,40 +94,80 @@ KeypadFunction();
 
   void getButtonPressedOfFloor(int floorCount){
   
-  Wire.requestFrom(floorCount,3);
+  Wire.requestFrom(floorCount,2);
 
   int number = Wire.read();
-  int ButtonUp = Wire.read();
-  int ButtonDown = Wire.read();
+  int ButtonState = Wire.read();
+  
   
     
    Serial.println(number);
-
-     Serial.println(ButtonUp);
-  Serial.println(ButtonDown);
+   Serial.println("BUTON UP IS");
+   Serial.println(ButtonState);
+   
   LiftLocation(number); 
- moveLift(ButtonUp, ButtonDown);
+  moveLift(ButtonState);
 
 
   
  }
 
 
- void moveLift(int x, int y){
+ void moveLift(int x){
 
 
-if (x == 0 && y == 1){
-    stepper.step(10000);
+
+
+if ( x == 1 || x == 6){
+
+  goToFloor = 0;
+
+
   
 }
 
-if (x == 2 && y == 2){
-   
-   
-      stepper.step(0);
+
+if ( x == 2 || x == 7){
+
+  goToFloor = 1;
+
 
   
 }
+
+
+if ( x == 3 || x == 8){
+
+  goToFloor = 2;
+
+
+  
+}
+
+
+if ( x == 4 || x == 9){
+
+  goToFloor = 3;
+
+
+  
+}
+
+
+
+if ( x == 5 || x == 10){
+
+  goToFloor = 4;
+
+
+  
+}
+
+
+
+
+
+
   
  }
 
@@ -97,8 +177,7 @@ if (x == 2 && y == 2){
     
 
   Wire.beginTransmission(floorCount);               
-  Wire.write(currentFloor);
-  Wire.write(DoorNumber);                                        
+  Wire.write(currentFloor);                                        
   Wire.endTransmission();                  
  
 
@@ -113,6 +192,7 @@ if (x == 2 && y == 2){
  if ( x == 1){
 
      currentFloor = 0;
+     
   
  }
  
@@ -138,34 +218,8 @@ if (x == 2 && y == 2){
   
  }
   }
- void openDoor(int x){
+  
 
-  if (x == 1){
-   
-    DoorNumber = 1;
-    
-  }  if (x == 2){
-
-    DoorNumber = 2;
-   
-  }
-   if (x == 3){
-
-    DoorNumber = 3;
-    
-  }  if (x == 4){
-
-    DoorNumber = 4;
-    
-  }
-   if (x == 5){
-
-    DoorNumber = 5;
-    
-  }
-
-
- }
 
   void KeypadFunction(){
 
@@ -173,37 +227,127 @@ char key = keypad.getKey();
 
   if (key == '0'){
 
-     
-  stepper.step(-10000); 
    
-    openDoor(1);
+    goToFloor = 0;
+
+   
 
   }
 
   if (key == '1'){
-  
-    openDoor(2);
+     
+      
+      goToFloor = 1;
+
+    
 
   }
     if (key == '2'){
+
+
+
+     goToFloor = 2;
+
    
-    openDoor(3);
 
   } 
   
   if (key == '3'){
-  
-    openDoor(4);
+
+
+    goToFloor = 3;
+
+   
 
   }
     if (key == '4'){
- 
-    openDoor(5);
+
+
+   goToFloor = 4;
+
+   
+
+  }
 
   }
 
 
-    
+
+void moveLift() {
+  // Controls the lift controller
+  if (moveUp) {
+    Serial.println("going up");
+    liftController(true);
+  } else if (moveDown) {
+    Serial.println("going down");
+    liftController(false);
   }
+}
+
+
+
+void liftController(boolean up) {
+  // Controls how many the stepping motor should rotate
+  clockwise = !up;
+
+
+  for (int i = 0; i < 1000; i++) { // remove or increase when needed,
+    // with this for loop the motor moves more between requests.
+    stepper();
+    delayMicroseconds(2000);
+  }
+}
+
+void stepper() {
+  // controls the steppenmotor
+  switch (steps) {
+    case 0:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
+    case 1:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      break;
+    case 2:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, HIGH);
+      break;
+    case 3:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      break;
+    default:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
+  }
+
+  if (clockwise) {
+    steps++;
+  } else {
+    steps--;
+  }
+
+  if (steps > 3) {
+    steps = 0;
+  } else if (steps < 0) {
+    steps = 3;
+  }
+}
+
+
+
+  
 
   
